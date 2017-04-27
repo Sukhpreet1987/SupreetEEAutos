d01f897bcba493e68af54c89d4c0236a56158e2a
Sukhpreet1987 <d15123549@mydit.ie>
2017-04-27 14:15:06 +0100

    ok

---
+++ custom.js
@@ -0,0 +1,25 @@
+//alert('hello');
+
+$(document).ready(function(){
+	
+	$('#i-nav').click(function(){
+		
+		//$(this).toggleClass('open');
+	
+		$('ul').toggleClass('show');
+		
+		//alert('hello');
+		
+	});
+	
+	
+});
+//<script>
+function Check()
+	{
+		var make = document.stock.CarMake.value;
+		var choice = document.getElementById("hh");
+		choice.innerHTML = make;  <!--displays Telephone Number-->
+		
+	}
+</script>
